04758718a8fefee5b48558205d25b2f8e3ae4178
Tello Martinez <152498755+tello-martinez@users.noreply.github.com>
2024-03-16 14:07:48 -0400

    version1

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(('.skills'),{
+    strings : ['Hola Mundo','Bonjour le monde','Привіт, народ','Hallo Welt','ハローワールド','Hello World'],
+    backDelay : 800,
+    typeSpeed : 50,
+    backSpeed : 50,
+    backDelay : 900,
+    loop : true
+});
