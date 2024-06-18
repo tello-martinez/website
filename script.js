d335ab7637a7a758d1de02781ba904eb9849efe8
Tello Martinez <152498755+tello-martinez@users.noreply.github.com>
2024-06-17 21:35:05 -0400

    v2 commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var typed = new Typed(('.skills'),{
-    strings : ['Hola Mundo','Bonjour le monde','Привіт, народ','Hallo Welt','ハローワールド','Hello World'],
-    backDelay : 800,
-    typeSpeed : 50,
-    backSpeed : 50,
-    backDelay : 900,
-    loop : true
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
